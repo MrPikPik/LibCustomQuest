11014b758f692198db0ebd7cd0a30a9452f5f57a
Alianym <69038163+Alianym@users.noreply.github.com>
2020-08-12 15:16:32 +0800

    Update en.lua
    Removed "Talk" string and used the native in-game version: SI_GAMECAMERAACTIONTYPE2

--- lang/en.lua
+++ lang/en.lua
@@ -4,7 +4,6 @@ ZO_CreateStringId("SI_JOURNAL_CUSTOM_QUEST_MENU_HEADER", "Custom Quests")
 ZO_CreateStringId("SI_QUESTS_CURRENT", "Quests: |cffffff<<1>>|r")
 
 ZO_CreateStringId("SI_BINDING_NAME_ALIANYM_INTERACT_KEY", "Interaction Key")
-ZO_CreateStringId("ALIANYM_CUSTOM_DIALOGUE_ACTION", "Talk")
 ZO_CreateStringId("ALIANYM_LANGUAGE_NOT_SUPPORTED", "Language not supported!")
 
 --
