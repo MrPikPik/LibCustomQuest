0478689236e4b20869c54ed3eeb504dea4cad64a
MrPikPik <mrpikpikk@googlemail.com>
2021-04-27 10:59:25 +0200

    Listener: New Listener - CurrenyListener

---
+++ Listeners/CurrencyListener.lua
@@ -0,0 +1,50 @@
+-----------------------------------------
+-- Currency Listener
+-----------------------------------------
+
+LCQCurrencyListener = LCQListener:Subclass()
+
+function LCQCurrencyListener:New(...)
+    local listener = LCQListener.New(self, ...)
+    listener:Initialize(...)
+    return listener
+end
+
+function LCQCurrencyListener:Initialize(...)
+    self.name = "CurrencyListener"
+    self.targets = {}
+
+    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CURRENCY_UPDATE, function(...) self:Update(...) end)
+
+    LCQ_DBG:Verbose("Listener: Currency listener initalized")
+end
+
+function LCQCurrencyListener:Update(eventCode, currencyType, currencyLocation, newAmount, oldAmount, reason)
+    -- If the currency update is not the current character, bail
+    if currencyLocation ~= CURRENCY_LOCATION_CHARACTER then return end
+
+    -- If the player has lost money, bail
+    local difference = newAmount - oldAmount
+    if difference <= 0 then return end
+
+    -- No cheating :P
+    if  reason == CURRENCY_CHANGE_REASON_PLAYER_INIT or
+        reason == CURRENCY_CHANGE_REASON_BANK_WITHDRAWAL or
+        reason == CURRENCY_CHANGE_REASON_GUILD_BANK_WITHDRAWAL or
+        reason == CURRENCY_CHANGE_REASON_CASH_ON_DELIVERY or
+        reason == CURRENCY_CHANGE_REASON_MAIL or
+        reason == CURRENCY_CHANGE_REASON_JUMP_FAILURE_REFUND then
+            return
+    end
+
+    for _, target in ipairs(self.targets) do
+        if currencyType == target.currencyType then
+            target.amount = target.amount - difference
+
+            if target.amount <= 0 then
+                self:FireCallbacks("OnConditionMet", target)
+            end
+        end
+    end
+
+end
--- Listeners/ListenerBase.lua
+++ Listeners/ListenerBase.lua
@@ -12,22 +12,23 @@ function LCQListener:New(...)
 end
 
 function LCQListener:Initialize()
-    self.targets = {}
-    LCQ_DBG:Critical("Listener: Base listener class has been initialized as object!")
     -- To be overridden
+    self.targets = {}
+
+    LCQ_DBG:Critical("Listener: Base listener class has been initialized as object!")
 end
 
 function LCQListener:Listen(target, questId, conditionId)
     target.questid = questId
     target.conditionid = conditionId
     table.insert(self.targets, target)
-    LCQ_DBG:Verbose("<<1>>: Added new listening target for quest \"<<2>>\": <<3>>", self.name, questId, target.name)
+    LCQ_DBG:Verbose("<<1>>: Added new listening target for quest \"<<2>>\": <<3>>", self.name, questId, target.name or "Unnamed target")
 end
 
 function LCQListener:Remove(target)
     for i, t in ipairs(self.targets) do
         if t.questid == target.questid then
-            LCQ_DBG:Verbose("<<1>>: Removed all listening targets for \"<<2>>\": <<3>>", self.name, target.questId, target.name)
+            LCQ_DBG:Verbose("<<1>>: Removed all listening targets for \"<<2>>\": <<3>>", self.name, target.questId, target.name or "Unnamed target")
             table.remove(self.targets, i)
         end
     end
