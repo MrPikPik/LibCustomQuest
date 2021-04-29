ce6122252c1c1714266d86c5517e8929d75cb517
MrPikPik <mrpikpikk@googlemail.com>
2021-04-29 16:32:44 +0200

    Debugger update v1.4

--- LCQ_Debugger.lua
+++ LCQ_Debugger.lua
@@ -2,7 +2,7 @@
 -- LCQ_Debugger
 ----------
 
---debugger version 1.3.1
+--debugger version 1.4
 
 LCQ_DBG_QUIET       = 0
 LCQ_DBG_NORMAL      = 1
@@ -10,7 +10,8 @@ LCQ_DBG_INFO        = 2
 LCQ_DBG_VERBOSE     = 3
 LCQ_DBG_ERROR       = 4
 LCQ_DBG_CRITICAL    = 5
-LCQ_DBG_DEBUG       = 6
+LCQ_DBG_ALWAYS_SHOW = 6
+LCQ_DBG_DEBUG       = 7
 
 
 LCQ_Debugger = ZO_Object:Subclass()
@@ -23,16 +24,22 @@ end
 
 function LCQ_Debugger:Initialize()
     self.logLevel = LCQ_DBG_NORMAL
+    self.showDebug = false
 end
 
 function LCQ_Debugger:SetLogLevel(level)
     if level < LCQ_DBG_QUIET then
         self.logLevel = LCQ_DBG_QUIET
-    elseif level > LCQ_DBG_DEBUG then
+    elseif level >= LCQ_DBG_DEBUG then
         self.logLevel = LCQ_DBG_DEBUG
+        self.showDebug = true
     else
         self.logLevel = level
     end
+end
+
+function LCQ_Debugger:SetDebugOutputEnabled(enabled)
+    self.showDebug = enabled
 end
 
 function LCQ_Debugger:GetLogLevel()
@@ -42,7 +49,7 @@ end
 function LCQ_Debugger:Log(message, debugLevel, ...)
     if not message then return end
     local level = debugLevel or 1
-    if level <= self.logLevel then
+    if level >= self.logLevel or level == LCQ_DBG_ALWAYS_SHOW then
         local lvlstr = "|cffffff"
         if level == LCQ_DBG_INFO then
             lvlstr = "|c0fb800[Info] "
@@ -56,6 +63,7 @@ function LCQ_Debugger:Log(message, debugLevel, ...)
             lvlstr = "|c0081b8[Debug] "
         end
         
+        if level == LCQ_DBG_DEBUG and not self.showDebug then return end
         d("[LCQ Debug] " .. lvlstr .. zo_strformat(message, ...) .. "|r")
     end
 end
