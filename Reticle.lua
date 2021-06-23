72ef332f032fc0d2fc9b72d25d9a1e17b6f12d13
Alianym <Alianym@users.noreply.github.com>
2021-06-23 20:27:34 +0800

    Reticle Integration – requires LibAlianym

--- Reticle.lua
+++ Reticle.lua
@@ -1,51 +1,38 @@
 local LibCustomQuest = LibCustomQuest or {}
 
-local function UpdateInteractText(self, currentFrameTimeSeconds)
-    local name = GetInteractionTargetName()
-    if LCQ_INTERACTIONLISTENER:IsTargetRegisteredInteraction(name) then
-        -- If there is no interaction enabled from the base game, we need to enable and update the interaction paramters ourselves
-        if self.interact:IsHidden() then
-            self.interact:SetHidden(false)
-            self.interactContext:SetText(name)
+local function ReticleOverrides()
+    function LCQ_TEST_RETICLE:IsInteractableFoundNotFound(interactionExists)
+        local name = GetInteractionTargetName()
+        if (not name or name == "") and not IsUnitPlayer("reticleover") then
+            name = GetUnitName("reticleover")
         end
 
-        local isVanillaInteractionAvailable = GetGameCameraInteractableInfo()
-        if not isVanillaInteractionAvailable then
-            self.interactKeybindButton:SetHidden(true)
+        if interactionExists and LCQ_INTERACTIONLISTENER:IsTargetRegisteredInteraction(name) then
+            local result, name = true, name
+            local action = LCQ_INTERACTIONLISTENER:GetTargetInteractionText(name)
+
+            return true, name, action
         end
 
-        
-        self.interactKeybindButton2:SetHidden(false)
-        local orignalInteractionShown = self.interactKeybindButton:IsHidden()
-        if orignalInteractionShown then
-            self.interactKeybindButton2:ClearAnchors()
-            self.interactKeybindButton2:SetAnchor(TOPLEFT, ZO_ReticleContainerInteractContext, BOTTOMLEFT, 60, 6)
-        else
-            self.interactKeybindButton2:ClearAnchors()
-            self.interactKeybindButton2:SetAnchor(TOPLEFT, ZO_ReticleContainerInteractContext, BOTTOMLEFT, 60, 46)
-        end
-        self.interactKeybindButton2:SetText(LCQ_INTERACTIONLISTENER:GetTargetInteractionText(name))
-    else
-        self.interactKeybindButton:SetHidden(false)
-        self.interactKeybindButton2:SetHidden(true)
+        return false
     end
 end
 
-local function OnGamePadPreferredModeChange()
-    if IsInGamepadPreferredMode() then   
-        RETICLE.interactKeybindButton2:SetNameFont("ZoFontGamepad42")
-        RETICLE.interactKeybindButton2:SetupStyle(KEYBIND_STRIP_GAMEPAD_STYLE)
-    else
-        RETICLE.interactKeybindButton2:SetNameFont("ZoInteractionPrompt")
-        RETICLE.interactKeybindButton2:SetupStyle(KEYBIND_STRIP_STANDARD_STYLE)
+local function OnInteract()
+    if LCQ_TEST_RETICLE:IsThisReticleInstanceValidShowing() then
+        local interactionTargetName = LCQ_TEST_RETICLE.contextTarget
+
+        LCQ_INTERACTIONLISTENER:RunInteractionForTarget(interactionTargetName)
     end
+end
+
+local function ReticleSetFuncs()
+	LCQ_TEST_RETICLE:SetOnInteractFunc(LibCustomQuest.name, OnInteract)
+    LCQ_TEST_RETICLE:SetCustomInteractActionAndKeys(nil,"ALCI_INTERACT_KEY","ALCI_INTERACT_KEY")
 end
 
 function LibCustomQuest.SetupReticle()
-    RETICLE.interactKeybindButton2 = WINDOW_MANAGER:CreateControlFromVirtual("LCQ_InteractKeybindButton", ZO_ReticleContainerInteract, "ZO_KeybindButton")
-    RETICLE.interactKeybindButton2:SetKeybind("LCQ_INTERACT", true, "LCQ_INTERACT")
-
-    EVENT_MANAGER:RegisterForEvent("LCQ_Reticle", EVENT_GAMEPAD_PREFERRED_MODE_CHANGED, OnGamePadPreferredModeChange)
-
-    ZO_PostHook(RETICLE, "UpdateInteractText", UpdateInteractText)
+    LCQ_TEST_RETICLE = ALCINamespace:New(LibCustomQuest.name, ALCI_ON_INTERACT_FOUND_NOT_FOUND)
+    ReticleOverrides()
+	ReticleSetFuncs()
 end
